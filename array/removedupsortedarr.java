668df8354d3678dc0ac9ba3f5e9ad6d9939f5b94
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-07-17 00:00:33 +0530

    initial commit

---
+++ array/removedupsortedarr.java
@@ -0,0 +1,19 @@
+public class removedupsortedarr {
+    public static void main(String[] args) {
+        int arr[] = { 0, 0, 0, 1, 1, 2, 2, 3, 4, 5 };
+        int n = arr.length;
+        System.out.println(removeDuplicates(n, arr));
+    }
+
+    public static int removeDuplicates(int n, int arr[]) {
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+
+            }
+        }
+        return i + 1;
+    }
+}
