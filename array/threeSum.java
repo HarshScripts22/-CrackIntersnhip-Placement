99e12a1f46e87a36e63492511889313ad30bd2b1
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-07-25 23:15:28 +0530

    day10

--- array/threeSum.java
+++ array/threeSum.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 
 public class threeSum {
     public static void main(String[] args) {
-        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        int[] nums = { 3, 0, -2, -1, 1, 2 };
         System.out.println(findthreeSum(nums));
 
     }
@@ -13,22 +12,69 @@ public class threeSum {
     public static ArrayList<ArrayList<Integer>> findthreeSum(int nums[]) {
         int n = nums.length;
 
-        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        // HashSet<ArrayList<Integer>> set = new HashSet<>();
+        // for (int i = 0; i < n; i++) {
+        // for (int j = i + 1; j < n; j++) {
+        // for (int k = j + 1; k < n; k++) {
+        // if (nums[i] + nums[j] + nums[k] == 0) {
+        // ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[j],
+        // nums[k]));
+        // temp.sort(null);
+        // set.add(temp);
+
+        // }
+
+        // }
+        // }
+        // }
+        // ArrayList<ArrayList<Integer>> list = new ArrayList<>(set);
+        // return list;
+
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        HashSet<Long> set = new HashSet<>();
+        int target = 0;
+
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k]));
-                        temp.sort(null);
-                        set.add(temp);
+            int left = i + 1;
+            int right = n - 1;
+
+            int ntar = target - nums[i];
+
+            while (left < right) {
+                if (nums[left] + nums[right] < ntar) {
+                    left++;
+
+                } else if (nums[left] + nums[right] > ntar) {
+                    right--;
+
+                } else {
+                    long hash = getHash(nums[i], nums[left], nums[right]);
+                    if (set.contains(hash) == false) {
+                        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
+                        ans.add(list);
+                        set.add(hash);
 
                     }
 
+                    left++;
+                    right--;
+
                 }
+
             }
         }
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>(set);
-        return list;
+        return ans;
 
+    }
+
+    public static long getHash(int x, int y, int z) {
+        long hash = x;
+        hash *= 100000;
+        hash += y;
+        hash *= 100000;
+        hash += z;
+
+        return hash;
     }
 }
