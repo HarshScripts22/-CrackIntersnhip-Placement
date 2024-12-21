aca78da0f07a7ce4a7f98dbdb91ef50d39b06fc7
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-12-21 23:59:49 +0530

    searching algo

---
+++ searching/binarySearch.java
@@ -0,0 +1,28 @@
+// package searching;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int target = 6;
+        int ans = bs(arr, target);
+        System.out.println(ans);
+    }
+
+    public static int bs(int arr[], int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = high - (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+}
